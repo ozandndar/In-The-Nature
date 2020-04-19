572c7664818db1fb585ee4eb4c24950621f19c57
ozandndar <ozn.dndr921@gmail.com>
2020-04-19 12:38:34 +0000

    update db env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	flash = require('connect-flash'),
-	methodOverride 		= require("method-override"),
+	methodOverride = require('method-override'),
 	passport = require('passport'),
 	LocalStrategy = require('passport-local'),
 	passportLocalMongoose = require('passport-local-mongoose'),
@@ -13,33 +13,48 @@ var express = require('express'),
 	seedDB = require('./seeds');
 
 // Modals
-	Comment = require('./models/comment'),
-	Campground = require('./models/campground'),
-	User = require('./models/User');
+(Comment = require('./models/comment')),
+	(Campground = require('./models/campground')),
+	(User = require('./models/User'));
 
 // Routes
-var commentRoutes 		= require("./routes/comments"),
-		campgroundRoutes 	= require("./routes/campgrounds"),
-		authRoutes			 	= require("./routes/auth");
+var commentRoutes = require('./routes/comments'),
+	campgroundRoutes = require('./routes/campgrounds'),
+	authRoutes = require('./routes/auth');
 
 // APP CONFIG
-// seedDB();
-mongoose.connect('mongodb+srv://ozan9211:8f15xe7n@inthenature-2i0mh.mongodb.net/test?retryWrites=true&w=majority', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log("connected to db!");
-}).catch(err => {
-	console.log('ERROR:', err.message);
-});
+
+mongoose
+	.connect(process.env.DatabaseURL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then(() => {
+		console.log('connected to db!');
+	})
+	.catch(err => {
+		console.log('ERROR:', err.message);
+	});
+
+// mongoose.connect('mongodb+srv://ozan9211:8f15xe7n@inthenature-2i0mh.mongodb.net/test?retryWrites=true&w=majority', {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	useCreateIndex: true
+// }).then(() => {
+// 	console.log("connected to db!");
+// }).catch(err => {
+// 	console.log('ERROR:', err.message);
+// });
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(flash());
 app.use(expressSanitizer());
-app.use(function(req, res, next) { // set permissions for json endpoints
+app.use(function(req, res, next) {
+	// set permissions for json endpoints
 	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
@@ -62,18 +77,14 @@ passport.deserializeUser(User.deserializeUser());
 // send current user to all pages
 app.use((req, res, next) => {
 	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
 	next();
 });
 
 // Routes
 app.use(authRoutes);
-app.use("/kamp-yerleri",campgroundRoutes);
-app.use("/kamp-yerleri/:id/comments", commentRoutes);
-
-// app.listen(20000, function() {
-// 	console.log('App is running... Great!');
-// });
+app.use('/kamp-yerleri', campgroundRoutes);
+app.use('/kamp-yerleri/:id/comments', commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP);
